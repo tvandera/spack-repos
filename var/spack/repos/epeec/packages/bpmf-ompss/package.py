26c02016baa314e22beb9671c7ead62756c909dc
Tom Vander Aa <vanderaa@imec.be>
2022-01-26 13:42:38 +0100

    WIP: working BPMF+OmpSS@ArgoDSM

---
+++ var/spack/repos/epeec/packages/bpmf-ompss/package.py
@@ -0,0 +1,42 @@
+from spack import *
+import spack.util
+import os
+
+class BpmfOmpss(MakefilePackage):
+    """Julia and C++ implementations of Bayesian Probabilistic Matrix
+    Factorization using Markov Chain Monte Carlo."""
+
+    homepage = "https://github.com/ExaScience/bpmf"
+    git      = "https://github.com/ExaScience/bpmf"
+
+    maintainers = ['tvandera', ]
+
+    version('ompss',  branch='ompss')
+
+    # numlatent = [2, 4, 8, ..., 128]
+    variant(
+       'nl', default='64', description='number of latent dimensions',
+        values=[ str(2**i) for i in range(1,8) ], multi=False
+    )
+
+    variant('profile', default=True, description='Enable profiling')
+
+    depends_on('nanos6')
+    depends_on('mcxx', type='build')
+    depends_on('random123')
+    depends_on('eigen')
+    depends_on('zlib')
+
+    build_directory = 'makefiles'
+
+    @property
+    def build_targets(self):
+        return [
+            "mcc_bpmf",
+            "BPMF_NUMLATENT=%s" % self.spec.variants['nl'].value,
+        ]
+
+    def install(self, spec, prefix):
+        mkdir(prefix.bin)
+        for file in [ 'mcc_bpmf', 'extrae.xml', 'nanos6.toml', 'trace.sh' ]:
+            install(os.path.join(self.build_directory, file), prefix.bin)
